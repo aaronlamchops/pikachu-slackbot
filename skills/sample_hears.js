302544d2ada0cea17e699be7bcd2c64a8df2ddc1
Aaron Lam <allambchops@gmail.com>
2018-04-26 09:26:47 -0600

    updated with help message, more commands, better handling

--- skills/sample_hears.js
+++ skills/sample_hears.js
@@ -44,7 +44,16 @@ module.exports = function(controller) {
     });
 
     controller.hears(['^say (.*)','^say'], 'direct_message,direct_mention', function(bot, message) {
-        bot.reply(message, yesOrNo[1])
+        bot.startConversation(message, function(err, convo) {
+            convo.ask('Why would you try to make me do that?', function(response, convo) {
+
+                convo.say('Well... im still not going to say what you want me to say');
+                convo.say('Even if you are Mark Findlay...');
+                convo.say('#NotYourTalkingPikachu #EnoughIsEnough');
+                convo.next();
+
+            });
+        });
     });
   
   
@@ -58,27 +67,70 @@ module.exports = function(controller) {
   
     const feelings = ['https://gph.is/1JcmX5S', 'https://gph.is/NdSyj3', 'https://gph.is/1sFtRAB', 'https://gph.is/28Lcsyf', 'https://gph.is/XJ2aVC', 'https://gph.is/1Z6ImrI', 'https://gph.is/XMGrMA'];
   
+    const ronSwanson = ['Keep your tears in your eyes where they belong.',
+                        'There\'s only one thing I hate more than lying: skim milk. Which is water that is lying about being milk.', 
+                        'Dear frozen yogurt, you are the celery of desserts. Be ice cream or be nothing. Zero stars.',
+                        'I\'d wish you the best of luck but I believe luck is a concept created by the weak to explain their failures.',
+                        'Child labor laws are ruining this country.',
+                        'I\'m not interested in caring about people.',
+                        'Crying: Acceptable at funerals and the Grand Canyon.',
+                        'Clear alcohols are for rich women on diets.',
+                        'Just give me all the bacon and eggs you have. Wait…wait. I worry what you just heard was: Give me a lot of bacon and eggs. What I said was: Give me all the bacon and eggs you have. Do you understand\?',
+                        'I once worked with a guy for three years and never learned his name. Best friend I ever had. We still never talk sometimes.',
+                        'Fishing relaxes me. It’s like yoga, except I still get to kill something.',
+                        'Give a man a fish and feed him for a day. Don’t teach a man to fish…and feed yourself. He’s a grown man. And fishing’s not that hard.',
+                        'On my deathbed, my final wish is to have my ex-wives rush to my side so I can use my dying breath to tell them both to go to hell one last time.',
+                        'When people get too chummy with me I like to call them by the wrong name to let them know I don\'t really care about them.',
+                        'There has never been a sadness that can’t been cured by breakfast food.',
+                        'It\'s always a good idea to demonstrate to your coworkers that you are capable of withstanding a tremendous amount of pain.',
+                        'Normally, if given the choice between doing something and nothing, I’d choose to do nothing. But I will do something if it helps someone else do nothing. I’d work all night, if it meant nothing got done.',
+                        'Give 100%. 110% is impossible. Only idiots recommend that.',
+                        'Capitalism: God\'s way of determining who is smart and who is poor.',
+                        'Birthdays were invented by Hallmark to sell cards.',
+                        'Friends: one to three is sufficient.',
+                        'I like saying ‘No,’ it lowers their enthusiasm.',
+                        'Sting like a bee. Do not float like a butterfly. That\'s ridiculous.'];
+
+    // help message
+    controller.hears(['^Help', '^help', '^-help', '^--help'], 'direct_message,direct_mention', function(bot, message) {
+        bot.reply(message, 'laugh\t\t\t - ill laugh for you\n' +
+                            'question, <inser question>?\t\t\t- ill respond with yes or no gifs\n' +
+                            'What do you think of <something>\t\t\t- ill respond with my feelings using gif\'s\n' +
+                            'Cant Stop The Music\t\t\t- ill play a nice video for you...\n' + 
+                            'Ron Swanson Advice\t\t\t- ill provide advice directly from Ron Swanson himself\n' + 
+                            'uptime\t\t\t- displays how long i have been up\n');
+    });
+
+    controller.hears(['^Aaron', '^aaron'], 'direct_message,direct_mention', function(bot, message) {
+        bot.reply(message, 'He is my Trainer!')
+    });
   
-//     controller.hears(['A-aron'], 'direct_message,direct_mention', function(bot, message) {
-//         bot.reply(message, 'He is my Trainer!')
-//     });
+    controller.hears(['^mark', '^Mark'], 'direct_message,direct_mention', function(bot, message) {
+        bot.reply(message, 'Booooooo to him! \nhttps://gph.is/XJ2aVC')
+    });
   
-//     controller.hears(['mark'], 'direct_message,direct_mention', function(bot, message) {
-//         bot.reply(message, 'Booooooo to him! \nhttps://gph.is/XJ2aVC')
-//     });
-  
-    controller.hears(['laugh'], 'direct_message,direct_mention', function(bot, message) {
+    controller.hears(['^laugh', '^Laugh'], 'direct_message,direct_mention', function(bot, message) {
         bot.reply(message, 'https://gph.is/XJ2aVC')
     });
   
   //  ^question, .+\? REGEX for a 'question, [text]?'
-    controller.hears(['^question, .+\?'], 'direct_message,direct_mention', function(bot, message) {
+    controller.hears(['^question, .+\?', '^Question, .+\?'], 'direct_message,direct_mention', function(bot, message) {
         bot.reply(message, yesOrNo[getRandomInt(4)])
     });
   
   // ^What do you think of .+\?
-    controller.hears(['^What do you think of .+\?'], 'direct_message,direct_mention', function(bot, message) {
+    controller.hears(['^What do you think of .+\?', '^What do you think about .+\?', '^what do you think of .+\?'], 'direct_message,direct_mention', function(bot, message) {
         bot.reply(message, feelings[getRandomInt(7)])
+    });
+
+    //https://www.youtube.com/watch?v=1svA2sGhDEE&
+    controller.hears(['^Cant Stop The Music', '^Cant Stop the Music', '^Cant stop the music', '^cant stop the music', '^Can\'t Stop the Music'], 'direct_message,direct_mention', function(bot, message) {
+        bot.reply(message, 'https://www.youtube.com/watch?v=1svA2sGhDEE&')
+    });
+
+    // swanson jokes
+    controller.hears(['Ron Swanson Advice', 'ron swanson advice', 'Ron Swanson advice', 'Ron swanson advice'], 'direct_message,direct_mention', function(bot, message) {
+        bot.reply(message, ronSwanson[getRandomInt(23)])
     });
 
 
